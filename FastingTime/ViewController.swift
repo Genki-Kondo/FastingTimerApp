51e97a7ec1ad035d937d1a4de5957d2ac030f78e
近藤元気 <genki7onepiece@gmail.com>
2021-05-29 20:31:46 +0900

    Initial Commit

---
+++ FastingTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastingTime
+//
+//  Created by 近藤元気 on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
